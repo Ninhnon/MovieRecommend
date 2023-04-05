64a36fcd6702cd04261175ffddc36dea25eba27c
AmosIvor <trantuanvu00000@gmail.com>
2023-04-05 22:44:36 +0700

    Update: Back original status in App.js

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import MainNavigator from './src/navigation/navigation';
 import Description from './src/screens/Description';
-import Profile from './src/screens/Profile';
-import Catalogue from './src/screens/Catalogue';
-import History from './src/screens/History';
-import Favorite from './src/screens/Favorite';
 const App = props => {
-  // const {navigation} = props;
-  // return <MainNavigator />;
-  return <History />;
+  const {navigation} = props;
+  return <MainNavigator />;
 };
 export default App;
